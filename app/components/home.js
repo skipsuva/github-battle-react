16a51612f5056d4db9a21bbe782eff5e928317c9
skipsuva <skip.suva@gmail.com>
2016-07-31 15:55:23 -0400

    add files

---
+++ app/components/home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function () {
+    return(
+      <div> Hello from home route </div>
+    );
+  }
+
+});
+ module.exports = Home;
